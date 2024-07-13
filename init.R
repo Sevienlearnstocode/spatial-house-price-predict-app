f4afed1f193c334a9525ddf0fc9698bf27ca8f96
Sevienlearnstocode <60321314+Sevienlearnstocode@users.noreply.github.com>
2024-07-13 14:20:22 +0700

    Add files via upload

---
+++ init.R
@@ -0,0 +1,16 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c("shiny", "shinythemes", "spData", "spdep", "scales", 
+                "dplyr", "rmarkdown", "markdown", "spatialreg",
+                "leaflet", "purrr", "readxl")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
